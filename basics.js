9969637d0911eb441590319ee0a554bd8e11eb50
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2024-05-27 13:46:55 +0530

    basics

---
+++ basics.js
@@ -0,0 +1,33 @@
+//program to print the marks of a student in an obj using for loop
+
+//let obj = {harry : 98, rohan : 70, akash :7}
+
+/*using for in loop
+for (let key in obj) {
+     console.log(key, obj[key]);   
+}
+
+//using for of loop
+for (let key of Object.keys(obj)) {
+    console.log(key, obj[key]);
+}*/
+
+//using for loop
+//for (let i = 0; i < Object.keys(obj).length; i++) {
+//    console.log(Object.keys(obj)[i], obj[Object.keys(obj)[i]]);
+//}
+
+//var a = b = c = 10;
+//console.log(a, b, c);
+
+//program to print "try again" until the user enters 100
+/*let num = 0;
+while (num != 100) {
+    num = parseInt(prompt("Enter a number"));
+    if (num == 100)
+        console.log("good");
+    else 
+        console.log("try again");
+}*/
+
+//func to find mean of 5 nums
