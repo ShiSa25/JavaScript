02fdf1316634932982fec592a1e0a00ef4e81ea1
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2024-05-28 00:13:05 +0530

    Create basics1.js

---
+++ basics1.js
@@ -0,0 +1,19 @@
+//js code to take input of a number in nodejs
+// alert, confirm, prompt
+//ES14--2023
+//variables => let var and const and how to name them 
+
+/*let b = {
+    "name": "shiv",
+    "age": 23,
+    "sport": "badminton"
+}
+
+console.log(b.age);
+b.gender = "male";
+
+b["task"] = "fucking";
+console.log(b["task"]);*/
+
+ //Create a terminal clock (HH:MM:SS)
+
