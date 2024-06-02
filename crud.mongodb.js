058dee0a0b232ab0071eaad6c02283bfca1c960b
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2024-06-03 01:55:55 +0530

    mongoose

---
+++ crud.mongodb.js
@@ -0,0 +1,37 @@
+use("main")
+
+/*console.log(db)
+
+db.createCollection("users")
+
+db.users.insertOne(
+    {
+        "aim" : "dev",
+        "salary": "100000"
+    }
+)
+
+db.users.insertMany (
+    [
+        {
+            "aim" : "dev",
+            "salary": "100000"
+        },
+        {
+            "aim" : "dev",
+            "salary": "100000"
+        }
+    ]
+
+)
+
+let a = db.users.find({"aim": "dev"})
+console.log(a)
+
+db.users.findOne({"aim": "dev"})*/
+
+//db.users.updateOne({"salary": "100000"}, {$set: {"salary": "1000000"}})
+//db.users.updateMany """"
+
+//db.users.deleteOne({"aim": "dev"})
+//db.users.deleteMany({"aim": "dev"})
