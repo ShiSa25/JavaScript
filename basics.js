3ec38cbf61d38caffeab047ae4de117dae6fea2f
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2025-04-01 23:23:00 +0530

    JavaScript concepts

--- basics.js
+++ basics.js
@@ -50,5 +50,58 @@ while (num != 100) {
 //     }    
 // }
 
+// console.log('1'%5);
+// console.log(1+'1');
 
+ const prompt = require('prompt-sync')();
+// let age = prompt("what is your age");
+// age = Number(age);
+// console.log(age);
 
+//Nan, type casting, Math.floor,ceil,round,trunc,pow,sqrt,abs,max,min,random,tofixed
+
+// let a= 20;
+// let b= 30;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// let a= 3472;
+// let b= a%10;
+// let c= Math.floor(a/10);
+
+// console.log(b,c);
+
+//console.log(13 === '13');
+
+// let a = true;
+// a++;
+// console.log(a);
+
+// let i = 11
+// let j = ++(++i);
+// console.log(j);
+
+//console.log(Math.trunc((Math.random() * 9000)+1000));
+
+// let unit = Number(prompt("enter units"));
+// let bill = 0;
+
+// if(unit>400) {
+//     bill = (unit-400) * 13;
+//     unit = 400;
+// } if(unit>200 && unit<=400) {
+//     bill += (unit-200) * 8;
+//     unit = 200;
+// } if(unit>100 && unit<=200) {
+//     bill += (unit-100) * 6;
+//     unit = 100;
+// } 
+// bill += unit *4;
+// console.log(bill);
+
+//ternary operator and nested ternary operator and switch cases
+console.log(typeof(x));
