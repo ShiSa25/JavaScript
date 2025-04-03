212d329515bb80cd0a0ea6b888a6f8d325538fb3
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2025-04-03 23:16:39 +0530

    loops2

--- loops.js
+++ loops.js
@@ -1,11 +1,22 @@
 //code to print sum of 1 to 1000 with AP
-function sumOfAP(a, d, n) {
+// function sumOfAP(a, d, n) {
+//     let sum = 0;
+//     for (let i = 0; i < n; i++) {
+//         sum += a + (i * d);
+//     }
+//     return sum;
+// }
+
+// sum = sumOfAP(1, 1, 1000);
+// console.log(sum);
+
+//code to print sum of 1 to 1000 with GP
+function sumOfGP(a, r, n) {
     let sum = 0;
     for (let i = 0; i < n; i++) {
-        sum += a + (i * d);
+        sum += a * Math.pow(r, i);
     }
     return sum;
 }
-
-sum = sumOfAP(1, 1, 1000);
+sum = sumOfGP(1, 2, 1000);
 console.log(sum);
