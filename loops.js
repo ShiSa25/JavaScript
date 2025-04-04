4d43ba22140c2ecfc13152438d693c025ac810a1
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2025-04-04 21:59:59 +0530

    fibo

--- loops.js
+++ loops.js
@@ -11,12 +11,28 @@
 // console.log(sum);
 
 //code to print sum of 1 to 1000 with GP
-function sumOfGP(a, r, n) {
-    let sum = 0;
-    for (let i = 0; i < n; i++) {
-        sum += a * Math.pow(r, i);
+// function sumOfGP(a, r, n) {
+//     let sum = 0;
+//     for (let i = 0; i < n; i++) {
+//         sum += a * Math.pow(r, i);
+//     }
+//     return sum;
+// }
+// sum = sumOfGP(1, 2, 1000);
+// console.log(sum);
+
+//code to generate fibonacci series
+function fibonacci(n) {
+    let a = 0;
+    let b = 1;
+    let c;
+    console.log(a);
+    console.log(b);
+    for (let i = 2; i < n; i++) {
+        c = a + b;
+        console.log(c);
+        a = b;
+        b = c;
     }
-    return sum;
 }
-sum = sumOfGP(1, 2, 1000);
-console.log(sum);
+fibonacci(10);
