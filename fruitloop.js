44789df1ace4ec2870d403f3ea94529357e31968
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2025-04-15 23:08:31 +0530

    push96

--- fruitloop.js
+++ fruitloop.js
@@ -77,3 +77,19 @@ for (let i = 1; i <= 1000; i++) {
         console.log(i);
     }
 }
+
+//code to print fibonacci series from 1 to 1000
+function fibonacci(num) {
+    let a = 0;
+    let b = 1;
+    let c;
+    console.log(a);
+    console.log(b);
+    for (let i = 2; i < num; i++) {
+        c = a + b;
+        console.log(c);
+        a = b;
+        b = c;
+    }
+}
+
