83176a6b6fabea5bc7375b5e79d90604f374bf7f
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2025-04-17 23:50:35 +0530

    push97

--- fruitloop.js
+++ fruitloop.js
@@ -93,3 +93,12 @@ function fibonacci(num) {
     }
 }
 
+//code to print factorial of a number
+function factorial(num) {
+    let fact = 1;
+    for (let i = 1; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
