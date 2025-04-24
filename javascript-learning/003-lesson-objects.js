35e8f42a99229aac2644d905fcd157d7f94f0d3e
Shivanshu Sahu <68166464+ShiSa25@users.noreply.github.com>
2025-04-24 23:59:18 +0530

    push78

--- javascript-learning/003-lesson-objects.js
+++ javascript-learning/003-lesson-objects.js
@@ -172,3 +172,6 @@ function isMobileNumberValid(number) {
 // fetch('https://jsonplaceholder.typicode.com/todos/1')
 //     .then(response => response.json())
 //     .then(json => console.log(json));
+
+//convert the json to string
+
